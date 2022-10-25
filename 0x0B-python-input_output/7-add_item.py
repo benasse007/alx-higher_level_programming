29f7b3f1df1cbb81a9c8462d204fdf55f1a3e8d7
ben <ferdinandkoli09@gmail.com>
2022-10-26 00:39:06 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-# 9-add_item.py
-# Brennan D Baraban <375@holbertonschool.com>
-"""Add all arguments to a Python list and save them to a file."""
+'''A script that stores arguments to a JSON file.
+'''
 import sys
+import os
+from importlib import import_module as using
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('8-load_from_json_file').load_from_json_file
 
-    try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+save_to_json_file, load_from_json_file = (
+    using('5-save_to_json_file').save_to_json_file,
+    using('6-load_from_json_file').load_from_json_file
+)
+'''The functions for saving and loading lists.
+'''
+args_list = []
+'''The list of arguments.
+'''
+args_list_file_name = 'add_item.json'
+'''The file name of the file containing the list of arguments.
+'''
+
+
+def run():
+    '''Runs the routines of this script.
+    '''
+    if not os.path.exists(args_list_file_name):
+        with open(args_list_file_name, mode='w', encoding='utf-8') as file:
+            file.write('[]')
+    json_list = load_from_json_file(args_list_file_name)
+    if (type(json_list) is list) and all(type(s) is str for s in json_list):
+        args_list.extend(json_list)
+    args_list.extend(sys.argv[1:])
+    save_to_json_file(args_list, args_list_file_name)
+
+
+if __name__ == '__main__':
+    run()
